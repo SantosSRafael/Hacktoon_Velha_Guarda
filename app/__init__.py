1ba27b38005353586add1802e8a2c3969e1e3725
Teylor Lucas A. Silva <45246094+TeylorLuks@users.noreply.github.com>
2023-07-03 18:40:48 -0300

    Ajuste de Pathing Structure

---
+++ app/__init__.py
@@ -0,0 +1,6 @@
+from app.modulo import ClasseA, funcao_a
+
+objeto = ClasseA("João")
+objeto.saudacao()  # Saída: "Olá, João!"
+
+funcao_a()  # Saída: "Função A executada"
---
+++ app/modulo.py
@@ -0,0 +1,9 @@
+class ClasseA:
+    def __init__(self, nome):
+        self.nome = nome
+
+    def saudacao(self):
+        print(f"Olá, {self.nome}!")
+
+def funcao_a():
+    print("Função A executada")
